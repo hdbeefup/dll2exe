1e0f5fac1adbc3ed3d00e5c4f68e2eeb6d375f47
quiret <quiret@5db80297-5349-47b6-8896-81534d2d820a>
2017-05-16 12:39:07 +0000

    - added "efix" commandline option to reput the old entrypoint RVA after EXE launch for version detection by certain DLL modules (.ASI)

---
+++ pefrmdllembed/src/option.h
@@ -0,0 +1,38 @@
+#ifndef _OPTION_UTILITIES_
+#define _OPTION_UTILITIES_
+
+#include <string>
+
+struct OptionParser
+{
+    OptionParser( const char *args[], size_t numArgs );
+    ~OptionParser( void );
+
+    std::string FetchOption( void );
+
+    inline size_t GetArgIndex( void ) const             { return this->curArg; }
+    inline const char* GetArgPointer( void ) const      { return this->curArgPtr; }
+
+private:
+    inline const char* UpdateArgPtr( size_t argIdx, size_t numArgs )
+    {
+        if ( argIdx < numArgs )
+        {
+            return this->args[ argIdx ];
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+
+    // Management variables.
+    const char **args;
+    size_t numArgs;
+
+    // Current iteration status.
+    size_t curArg;
+    const char *curArgPtr;
+};
+
+#endif //_OPTION_UTILITIES_
